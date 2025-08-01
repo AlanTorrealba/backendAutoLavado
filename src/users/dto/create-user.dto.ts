6fe3c2a1fb581dec582da703cc7cd6173369123f
ant <antorrealbatapia@gamil.com>
2025-08-01 00:29:59 -0400

    backend

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsOptional, IsArray } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  nombre: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsArray()
+  roleIds?: number[];
+}
